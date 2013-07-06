8b0f6050cd0d5a94c412b699060f40121a8d7d8a
Daniela Wellisz <daniela.wellisz@gmail.com>
2013-07-06 00:45:36 -0700

    added table numbers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,29 @@ module ApplicationHelper
   def view_box_offset(fib_count)
     [spiral_offset((fib_count+2)%4,fib_count),spiral_offset((fib_count+1)%4,fib_count)]
   end
+
+  def font_location(fib_count)
+    case fib_count
+    when 1,2
+      [-0.05,0.3]
+    when 3
+      [-0.25,-0.35]
+    when 4
+      [0.55,-0]
+    when 5
+      [0,0.5]
+    when 6
+      [-0.7,0.6]
+    when 10
+      [-3,0.5]
+    when 11
+      [-4,1]
+    when 13
+      [-15,0]
+    when 15
+      [-55,0]
+    else
+      [0,0]
+    end
+  end
 end
